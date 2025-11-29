a0e1498133ca3163f191d30d614cddb39c65bfb1
Thierry Bremard <t.bremard@gmail.com>
2025-11-29 17:07:04 +0100

    add console app to use lib

--- Dubz/Program.cs
+++ Dubz/Program.cs
@@ -1,6 +1,8 @@
 ﻿using DubzLib;
 using System;
 using System.IO;
+using System.Linq;
+using static System.Net.WebRequestMethods;
 
 namespace Dubz
 {
@@ -8,15 +10,38 @@ namespace Dubz
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length != 1)
             {
                 Console.WriteLine("Usage: Dubz <Directory>");
                 Console.WriteLine("      search for similar file recursively in <Directory>");
                 Console.WriteLine("      display all matching files");
                 return;
             }
-            string currentDirectory = Directory.GetCurrentDirectory();
+            string targetDir = args[0];
+            if (!Directory.Exists(targetDir))
+            {
+                Console.WriteLine($"Error: Directory '{targetDir}' does not exist.");
+                return;
+            }
             var dubletten = new Dubletten();
+            Console.WriteLine("[+] Grouping by size all files...");
+            var kandidaten = dubletten.SammleKandidaten(targetDir, Vergleichsmodi.Groesse);
+            Console.WriteLine("[+] Calculate hashes for exact matching...");
+            var found = dubletten.PruefeKandidaten(kandidaten);
+            if (found.Count==0)
+            {
+                Console.WriteLine("No duplicate files found.");
+                return;
+            }
+            foreach (var dublette in found)
+            {
+                Console.WriteLine("------------------");
+                foreach (var pfad in dublette.Dateipfade)
+                {
+                    Console.WriteLine($"  - {pfad}");
+                }
+            }
+            Console.WriteLine($"Found {found.Count} duplicate group of files");
         }
     }
 }
